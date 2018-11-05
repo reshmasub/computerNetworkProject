c4c13726eb4c22410e686ad666291a6e47ac975b
Reshma Subramaniam <Reshma@rs-macbook-air.local>
2018-11-05 12:10:39 -0800

    Initial Commit

---
+++ ProgrammingAssignment/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  ProgrammingAssignment
+//
+//  Created by Reshma Subramaniam on 05/11/18.
+//  Copyright © 2018 Reshma Subramaniam. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
